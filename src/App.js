6fb0e362eecf6b3daf4441cb356e9fae76c9a4df
maggieeagle <oryol.margo@gmail.com>
2023-12-05 23:24:49 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import ReactDOM from "react-dom/client";
-import {  HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {  BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NoPage from "./pages/NoPage.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import SignIn from './components/SignIn.jsx';
@@ -9,14 +9,14 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/GraphQL/">
+    <BrowserRouter basename="/GraphQL/">
       <Routes>
-        <Route path="/" element={<Navigate to="dashboard" replace />} />
-        <Route index path="dashboard" element={<Dashboard />} />
-        <Route path="login" element={<SignIn />} />
+        {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
+        <Route index path="/" element={<Dashboard />} />
+        <Route path="/login" element={<SignIn />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
