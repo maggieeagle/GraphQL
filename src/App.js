f2a32a2127ae371716fca399383918ecb9ecf23f
maggieeagle <71329681+maggieeagle@users.noreply.github.com>
2023-12-05 22:58:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import ReactDOM from "react-dom/client";
-import { HashROuter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {  HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import NoPage from "./pages/NoPage.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import SignIn from './components/SignIn.jsx';
@@ -8,14 +8,14 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/GraphQL/">
+    <HashRouter basename="/GraphQL/">
       <Routes>
         <Route path="/" element={<Navigate to="#/dashboard" />} />
         <Route index path="#/dashboard" element={<Dashboard />} />
         <Route path="#/login" element={<SignIn />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
